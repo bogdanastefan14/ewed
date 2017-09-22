f20499f99cbf44784e53619b8e49b1f72f96c46a
Furkan Aksoy <fufuceng@gmail.com>
2017-09-22 19:52:27 +0200

    Updated sanitize.php, added escape function.

--- functions/sanitize.php
+++ functions/sanitize.php
@@ -1,7 +1,21 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: fufu
  * Date: 22/09/2017
  * Time: 19:08
+ * @param $string
+ * @return string
  */
+
+/*
+ * htmlentites() function convert some characters to
+ * HTML entites. Using this function, we can prevent
+ * our systems from the some script injections, xss.
+ *
+ * We decided to using ENT_QUATES as a quote style.
+ * Because, we want to convert not only double quates
+ * but also single quates.
+ *
+ */
+function escape($string) {
+    return htmlentities($string, ENT_QUOTES, 'UTF-8');
+}
