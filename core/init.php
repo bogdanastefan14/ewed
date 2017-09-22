8eae5a7936219680edff5b8f8979f1b4082ea136
Furkan Aksoy <fufuceng@gmail.com>
2017-09-22 19:23:17 +0200

    Updated init.php, added spl_autoload_register class and global variables.

--- core/init.php
+++ core/init.php
@@ -3,3 +3,29 @@
  * Date: 22/09/2017
  * Time: 19:08
  */
+
+session_start();
+
+$GLOBALS['config'] = array(
+    'mysql' => array(
+        'host' => '127.0.0.1',
+        'username' => 'root',
+        'password' => 'root',
+        'db' => 'ewed'
+    ),
+    'remember' => array(
+        'cookie_name' => 'hash',
+        'cookie_expiry' => 604800
+    ),
+    'session' => array(
+        'session_name' => 'user'
+    )
+);
+
+spl_autoload_register(
+    function ($class) {
+        require_once '/classes/' . $class . '.php';
+    }
+);
+
+require_once '/functions/sanitize.php';
