e0bb7642feb8b5264a391d17ee061c6a5260b1ad
Furkan Aksoy <fufuceng@gmail.com>
2017-09-23 13:31:02 +0200

    Added put(), get(), exist(), delete() functions

--- classes/Session.php
+++ classes/Session.php
@@ -6,5 +6,21 @@
  */
 
 class Session {
+    public static function put($name, $value) {
+        return $_SESSION[$name] = $value;
+    }
 
+    public static function get($name) {
+        return $_SESSION[$name];
+    }
+
+    public static function exist($name) {
+        return isset($_SESSION[$name]);
+    }
+
+    public static function delete($name) {
+        if (self::exist($name)) {
+            unset($_SESSION[$name]);
+        }
+    }
 }
