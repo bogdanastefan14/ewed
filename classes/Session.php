2cd015447dfb721753ee258ce9f58bd2bd08237f
Furkan Aksoy <fufuceng@gmail.com>
2017-09-23 16:31:29 +0200

    added flash() method

--- classes/Session.php
+++ classes/Session.php
@@ -23,4 +23,17 @@ class Session {
             unset($_SESSION[$name]);
         }
     }
+
+    # Show flash message to the user
+    public static function flash($name, $content = null) {
+        if (self::exist($name)) {
+            # if this message exist, remove it
+            # because we want to see the message just one.
+            $session = self::get($name);
+            self::delete($name);
+            return $session;
+        } else {
+            self::put($name, $content);
+        }
+    }
 }
