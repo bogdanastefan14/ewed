264452c29d3fd7724d3353c1326b360ca9c79e9b
Furkan Aksoy <fufuceng@gmail.com>
2017-09-23 13:30:29 +0200

    Added generate() and check() functions

--- classes/Token.php
+++ classes/Token.php
@@ -5,5 +5,17 @@
  */
 
 class Token {
+    public static function generate() {
+        return Session::put(Config::get('session/token_name'), md5(uniqid()));
+    }
 
+    # Check the token exist in the session or not.
+    public static function check($token) {
+        $token_name = Config::get('session/token_name');
+        if (Session::exist($token_name) && $token === Session::get($token_name)) {
+            Session::delete($token_name);
+            return true;
+        }
+        return false;
+    }
 }
