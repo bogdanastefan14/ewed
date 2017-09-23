4b0ee8ae4ca8c577e275a05a9c7b82bc939e1d32
Furkan Aksoy <fufuceng@gmail.com>
2017-09-23 13:32:47 +0200

    Added exist() and get() function. exist() -> check submitted things exist get() -> get data from submitted things.

--- classes/Input.php
+++ classes/Input.php
@@ -5,5 +5,32 @@
  */
 
 class Input {
+    # Check input exists/submitted or not.
+    public static function exists($type) {
+        switch ($type) {
+            case 'post':
+                return (!empty($_POST));
+                break;
 
+            case 'get':
+                return (!empty($_GET));
+                break;
+
+            default:
+                return false;
+                break;
+        }
+    }
+
+    public static function get($item) {
+        if (isset($_POST[$item])) {
+            return $_POST[$item];
+        }
+
+        if (isset($_GET[$item])) {
+            return $_GET[$item];
+        }
+
+        return '';
+    }
 }
