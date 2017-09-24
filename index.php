90f50cd365b823583d57fdeba9082fc9f104873d
Furkan Aksoy <fufuceng@gmail.com>
2017-09-25 01:22:01 +0200

    Created basic index.php

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: fufu
  * Date: 22/09/2017
  * Time: 18:54
  */
+
+// Add init.php
+require_once 'core/init.php';
+
+if (Session::exist('success')) {
+    echo Session::flash('success');
+}
+
+$user = new User();
+
+if ($user->isLoggedIn()) {
+    echo "Hi, " . $user->getData()->username . " | ";
+    echo "<a href=\"logout.php\">Log out</a>";
+} else {
+    echo "You need to <a href=\"login.php\">Log in</a> or <a href=\"register.php\">Register</a>";
+}
+
+
