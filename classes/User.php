21d3373c1104e1ef6c2ecef3b5be18e8325ac510
Furkan Aksoy <fufuceng@gmail.com>
2017-09-24 23:55:45 +0200

    Added create() function

--- classes/User.php
+++ classes/User.php
@@ -5,5 +5,15 @@
  */
 
 class User {
+    private $_db;
 
+    public function __construct() {
+        $this->_db = DB::getInstance();
+    }
+
+    public function create($fields) {
+        if (!$this->_db->insert('users', $fields)) {
+            throw new Exception("There was a problem creating a account.");
+        }
+    }
 }
